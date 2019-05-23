3f0a3c3780cf832d5818549bee2dae99d8d46661
tesfaye.gari@gmail.com <tesfaye.gari@gmail.com>
2019-05-23 16:23:39 -0400

    Property added

--- src/webparts/reactWebpartDemo/ReactWebpartDemoWebPart.ts
+++ src/webparts/reactWebpartDemo/ReactWebpartDemoWebPart.ts
@@ -6,13 +6,17 @@ import {
   IPropertyPaneConfiguration,
   PropertyPaneTextField
 } from '@microsoft/sp-webpart-base';
-
+import {
+  PropertyFieldCollectionData,
+  CustomCollectionFieldType
+} from "@pnp/spfx-property-controls/lib/PropertyFieldCollectionData";
 import * as strings from 'ReactWebpartDemoWebPartStrings';
 import ReactWebpartDemo from './components/ReactWebpartDemo';
 import { IReactWebpartDemoProps } from './components/IReactWebpartDemoProps';
 
 export interface IReactWebpartDemoWebPartProps {
   description: string;
+  collectionData: any[];
 }
 
 export default class ReactWebpartDemoWebPart extends BaseClientSideWebPart<IReactWebpartDemoWebPartProps> {
@@ -27,7 +31,7 @@ export default class ReactWebpartDemoWebPart extends BaseClientSideWebPart<IReac
         currentSiteUrl: this.context.pageContext.web.absoluteUrl
       }
     );
-
+      console.log(this.properties.collectionData);
     ReactDom.render(element, this.domElement);
   }
 
@@ -48,6 +52,58 @@ export default class ReactWebpartDemoWebPart extends BaseClientSideWebPart<IReac
               groupFields: [
                 PropertyPaneTextField('description', {
                   label: strings.DescriptionFieldLabel
+                }),
+                PropertyFieldCollectionData("collectionData", {
+                  key: "collectionData",
+                  label: "Collection data",
+                  panelHeader: "Collection data panel header",
+                  manageBtnLabel: "Manage collection data",
+                  value: this.properties.collectionData,
+                  fields: [
+                    {
+                      id: "Title",
+                      title: "Firstname",
+                      type: CustomCollectionFieldType.string,
+                      required: true
+                    },
+                    {
+                      id: "Lastname",
+                      title: "Lastname",
+                      type: CustomCollectionFieldType.string
+                    },
+                    {
+                      id: "Age",
+                      title: "Age",
+                      type: CustomCollectionFieldType.number,
+                      required: true
+                    },
+                    {
+                      id: "City",
+                      title: "Favorite city",
+                      type: CustomCollectionFieldType.dropdown,
+                      options: [
+                        {
+                          key: "antwerp",
+                          text: "Antwerp"
+                        },
+                        {
+                          key: "helsinki",
+                          text: "Helsinki"
+                        },
+                        {
+                          key: "montreal",
+                          text: "Montreal"
+                        }
+                      ],
+                      required: true
+                    },
+                    {
+                      id: "Sign",
+                      title: "Signed",
+                      type: CustomCollectionFieldType.boolean
+                    }
+                  ],
+                  disabled: false
                 })
               ]
             }
